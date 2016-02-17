181a546ad2850465ca84e0a1efec8d5151cb494f
VICTOR CALDERON <vcalderon2009@my.fit.edu>
2016-02-16 21:59:44 -0600

    Reading data from sklearn.datasets.load_digits()

--- profiles/VictorC/code/DecisionTrees/VC_DecisionTrees.py
+++ profiles/VictorC/code/DecisionTrees/VC_DecisionTrees.py
@@ -4,3 +4,26 @@
 # Vanderbilt University
 # Project: Digits dataset for VandyAstroML project
 
+"""
+Description: Decision-tree algorithm on the dataset "Digits" from Scikit-learn.
+"""
+# modules for Python2/3
+from __future__ import division, print_function, absolute_import
+
+# Importing modules
+import numpy as num
+from sklearn import datasets
+
+def main():
+	## Loading dataset
+	digits = datasets.load_digits()
+	# Digits - Data
+	digits_dat = digits['data']
+	n_samples  = digits_dat.shape[0]
+	n_features = digits_dat.shape[1]
+	# Digits - target names
+	digits_tnames = digits['target_names']
+	# Digits - target values
+	digits_target = digits['target']
+
+
